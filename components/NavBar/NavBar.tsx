0fef695819be7bb17a8a47888d95fad4f400b258
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-03-11 23:13:06 +0800

    added some svg

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav className="bg-inherit border-b border-gray-300 border-opacity-30 w-full md:bg-inherit md:border-0 px-[96px] overflow-hidden py-5">
+        <h1 className=' font-bold size-[24px]'>CDBJ</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
