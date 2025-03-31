f2ed462a52dc9803510bce5ea435c713262242ed
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-03-31 21:36:23 +0800

    added sheetys

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import me from '../public/me.svg';
 import skills from '../public/skillset.svg';
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row px-[20px] overflow-hidden">
-      <main className="flex flex-col px-[20px] pl-[20px] justify-center items-center md:flex-row md:items-start">
+    <div className="flex flex-col md:flex-row px-[20px] md:justify-between overflow-hidden">
+      <main className="flex flex-col px-[20px] pl-[20px] justify-center items-center md:flex-row ">
         <div className="flex flex-row py-[20px]">
           <Image className="hidden md:show" src={img} width={40} height={40} alt="Arrow Animation" />
-          <div className="flex flex-col-reverse md:flex-row  md:px-[40px] md:pl-[40px] justify-center items-center md:items-start">
+          <div className="flex flex-col-reverse justify-center items-center md:flex-row md:justify-between md:items-end  md:px-[40px] md:pl-[40px] ">
             <div>
               <h1 className="text-center md:text-start text-[40px] md:text-[96px] font-bold">HEY THERE, IT&apos;S</h1>
               <h1 className="text-center md:text-start text-[40px] text-[#00ADB5] md:text-[96px] font-bold">
@@ -20,7 +20,7 @@ export default function Home() {
                     10000,
                     'A FREELANCER',
                     10000,
-                    'A FULLSTACK DEVELOPER',
+                    'A SOFTWARE DEVELOPER',
                     10000,
                     'AN INFO TECH STUDENT',
                     5000,
@@ -34,14 +34,16 @@ export default function Home() {
               </h1>
             </div>
             <div className="relative w-[250px] h-[250px]  rounded-full p-2 flex items-center justify-center">
-              <Image
-                className="absolute top-0  left-0 w-full h-full object-cover opacity-50"
-                src={skills}
-                alt="Background Image"
-              />
-              <Image className="relative w-[220px] h-[220px] object-contain rounded-full" src={me} alt="Profile Image" />
+              <div className=" absolute">
+                <Image
+                  className="absolute top-0  left-0 w-full h-full object-cover opacity-50"
+                  src={skills}
+                  alt="Background Image"
+                />
+                <Image className="relative w-[220px] h-[220px] object-contain rounded-full" src={me} alt="Profile Image" />
+                
+              </div>
             </div>
-
           </div>
         </div>
       </main>
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,10 +1,24 @@
+import { Menu } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet'
 
 function NavBar() {
   return (
     <div>
-        <nav className="bg-inherit border-b border-gray-300 border-opacity-30 w-full md:bg-inherit md:border-0 px-[96px] overflow-hidden py-5">
+        <nav className=" flex flex-row items-end justify-between bg-inherit border-b border-gray-300 border-opacity-30 w-full md:bg-inherit md:border-0 px-[96px] overflow-hidden py-5">
         <h1 className=' font-bold size-[24px]'>CDBJ</h1>
+        <Link href={''}  />
+        <Link href={''}/>
+        <Link href={''} />
+        <Sheet>
+          <SheetTrigger><Menu className='' /></SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[440px]">
+            <SheetHeader>
+              <SheetTitle>CDBJ</SheetTitle>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
   )
