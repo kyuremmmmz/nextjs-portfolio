063a6ac029c97f9e0a74e138a0c1d758f19eaf54
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-01 15:24:44 +0800

    done

--- components/AboutSection/About.tsx
+++ components/AboutSection/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <section id='#about' className='flex flex-col py-40'>
+    <section id='#about' className='flex flex-col py-10'>
           <div className='flex flex-row gap-2 px-60 lg:gap-8 md:py-[100px]'>
               <h1 className="text-center md:text-start text-[40px] md:text-[96px] font-bold">About</h1>
               <h1 className="text-center md:text-start text-[40px] text-[#00ADB5] md:text-[96px] font-bold">me</h1>
