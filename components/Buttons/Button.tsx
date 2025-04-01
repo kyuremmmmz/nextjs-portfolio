87c483d138c1f2e877eb883f0094e911e5cca999
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-01 13:06:58 +0800

    button componenty

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 function Button({componentName, componentFunction}: ButtonType) {
   return (
     <div className="relative">
-      <div className="absolute">
-        <button onClick={componentFunction}>{ componentName }</button>
+      <div className="absolute bg-[#00ADB5] rounded-3xl w-[129px] h-[47px] text-center items-center py-3 cursor-pointer">
+        <button className=' cursor-pointer w-full h-full' onClick={componentFunction}>{ componentName }</button>
       </div>
     </div>
   )
