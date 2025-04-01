30d059b17564c82d895f0b9c4929a278a7a91026
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-01 14:18:58 +0800

    navbar done

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -6,19 +6,23 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '../u
 function NavBar() {
   return (
     <div>
-        <nav className=" flex flex-row items-end justify-between bg-inherit border-b border-gray-300 border-opacity-30 w-full md:bg-inherit md:border-0 px-[96px] sm:px-[20px] overflow-hidden py-5">
+        <nav className=" flex flex-row items-end justify-between bg-inherit border-b border-gray-300 border-opacity-30 w-full md:bg-inherit md:border-0 px-[20px] lg:px-[296px] sm:px-[20px] overflow-hidden py-5">
         <h1 className=' font-bold size-[24px]'>CDBJ</h1>
-        <Link href={''}  />
-        <Link href={''}/>
-        <Link href={''} />
-        <Sheet>
-          <SheetTrigger><Menu className='' /></SheetTrigger>
-          <SheetContent className="w-[400px] sm:w-[440px]">
-            <SheetHeader>
-              <SheetTitle>CDBJ</SheetTitle>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
+        <div className='hidden sm:flex flex-row gap-8'>
+          <Link href={''}>Home</Link>
+          <Link href={''}>About</Link>
+          <Link href={''}>Projects</Link>
+        </div>
+        <div className='lg:hidden'>
+          <Sheet>
+            <SheetTrigger><Menu className='' /></SheetTrigger>
+            <SheetContent className="w-[400px] sm:w-[440px]">
+              <SheetHeader>
+                <SheetTitle>CDBJ</SheetTitle>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
       </nav>
     </div>
   )
