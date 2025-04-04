b195aa294152b1ddb20fc6fb97691a68e32ffd10
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-04 16:42:26 +0800

    techstack schemas

--- components/Badge/Skills.tsx
+++ components/Badge/Skills.tsx
@@ -1,44 +1,11 @@
 "use client"
 
-import { TechStackItem } from "@/types/TechStackType"
+import { techStack } from "@/types/TechStacks"
 import { useState } from "react"
 
 
 export default function TechStackTimeline() {
     const [activeIndex, setActiveIndex] = useState<number | null>(null)
-
-    const techStack: TechStackItem[] = [
-        {
-            year: "2024 - Present",
-            category: "Frontend Development",
-            technologies: ["Next.js", "React", "TypeScript", "Tailwind CSS", "Chakra UI", "Material UI",  "Bootstrap", "HTML & CSS"],
-            description: "Building modern, responsive web applications with focus on performance and user experience.",
-        },
-        {
-            year: "2024 - Present",
-            category: "Backend Development",
-            technologies: ["Node.js", "Express", "MongoDB", "PostgreSQL", "Supabase", "Firebase", "Laravel", "PHP"],
-            description: "Creating robust APIs and server-side applications with focus on scalability.",
-        },
-        {
-            year: "2024 - Present",
-            category: "Languages",
-            technologies: ["JavaScript", "TypeScript", "Python", "PHP"],
-            description: "Creating robust APIs and server-side applications with focus on scalability.",
-        },
-        {
-            year: "2024 - Present",
-            category: "Mobile Development",
-            technologies: ["Flutter", "Dart", "React Native"],
-            description: "Developing cross-platform mobile applications with a focus on performance and user experience.",
-        },
-        {
-            year: "2024 - Present",
-            category: "Tools",
-            technologies: ["Git", "Docker", "Github", "CI/CD"],
-            description: "Implementing deployment pipelines and infrastructure management for web applications.",
-        },
-    ]
 
     return (
         <div className="p-6 bg-inherit text-white max-w-4xl mx-auto">
---
+++ types/TechStacks.ts
@@ -0,0 +1,34 @@
+import { TechStackItem } from "./TechStackType";
+
+export const techStack: TechStackItem[] = [
+    {
+        year: "2024 - Present",
+        category: "Frontend Development",
+        technologies: ["Next.js", "React", "TypeScript", "Tailwind CSS", "Chakra UI", "Material UI", "Bootstrap", "HTML & CSS"],
+        description: "Building modern, responsive web applications with focus on performance and user experience.",
+    },
+    {
+        year: "2024 - Present",
+        category: "Backend Development",
+        technologies: ["Node.js", "Express", "MongoDB", "PostgreSQL", "Supabase", "Firebase", "Laravel", "PHP"],
+        description: "Creating robust APIs and server-side applications with focus on scalability.",
+    },
+    {
+        year: "2024 - Present",
+        category: "Languages",
+        technologies: ["JavaScript", "TypeScript", "Python", "PHP"],
+        description: "Creating robust APIs and server-side applications with focus on scalability.",
+    },
+    {
+        year: "2024 - Present",
+        category: "Mobile Development",
+        technologies: ["Flutter", "Dart", "React Native"],
+        description: "Developing cross-platform mobile applications with a focus on performance and user experience.",
+    },
+    {
+        year: "2024 - Present",
+        category: "Tools",
+        technologies: ["Git", "Docker", "Github", "CI/CD"],
+        description: "Implementing deployment pipelines and infrastructure management for web applications.",
+    },
+]
