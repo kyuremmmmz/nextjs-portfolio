b4ff767c2d092f4d5126c9a4efbb0ff7252fd088
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-05 21:59:12 +0800

    added

--- components/Sections/Projects.tsx
+++ components/Sections/Projects.tsx
@@ -10,21 +10,21 @@ function Projects() {
     const projects = ProjectsKo;
     const router = useRouter();
   return (
-      <section id="about" className="flex flex-col py-10 px-[20px] lg:px-60 items-center justify-center lg:flex-col lg:items-center lg:justify-center lg:py-16">
+      <section id="about" className="flex flex-col py-10 px-[10px] lg:px-60 md:px-[200px] items-center justify-center lg:flex-col lg:items-center lg:justify-center lg:py-16">
           <Header title="My" title2="Projects"/>
           <div className=' grid grid-cols-1 gap-6 py-10 lg:grid-cols-2 lg:px-4 lg:items-center  lg:gap-11 lg:justify-center'>
               {projects.map((project, index) => (
                   <div onClick={() => {
                       router.push(project.github);
-                  }} key={index} className=' bg-[#1a202c] overflow-hidden cursor-pointer hover:shadow-lg duration-300 shadow-blue-400 h-full w-full rounded-3xl'>
+                  }} key={index} className=' bg-[#1a202c] overflow-hidden  cursor-pointer hover:shadow-lg duration-300 shadow-blue-400 h-full w-full rounded-3xl'>
                       <div className='flex flex-col gap-2 lg:gap-8'>
                           <div className='flex flex-col gap-2 lg:gap-8'>
                               <div className=' w-full h-full overflow-hidden'>
                                   <Image src={project.image} className='w-full h-[150px] lg:h-[400px]'  width={0} height={0} alt={''}/>
                               </div>
-                              <div className='flex flex-col gap-2 lg:gap-8 lg:px-10 px-3'>
-                                  <h1 className="lg:text-center text-[16px] md:text-start lg:text-[20px] md:text-[20px] font-bold">{project.title}</h1>
-                                  <p className="lg:text-center md:text-start text-[14px] md:text-[16px] break-words whitespace-normal">{project.description}</p>
+                              <div className='flex flex-col gap-2 lg:gap-2 lg:px-10 px-3'>
+                                  <h1 className="lg:text-start text-[16px] md:text-start lg:text-[20px] md:text-[20px] font-bold">{project.title}</h1>
+                                  <p className="lg:text-start md:text-start text-[14px] md:text-[16px] break-words whitespace-normal">{project.description}</p>
                               </div>
                               <div className='flex flex-row flex-wrap gap-2 lg:gap-8 lg:px-10 pb-10 px-3'>
                                   {project.technologies.map((tech, index) => (
