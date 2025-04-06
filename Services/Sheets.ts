697ca5e3b56ea0ac4a5a54196df8b5c5489c1a2a
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-04-06 23:00:51 +0800

    done

--- Services/Sheets.ts
+++
@@ -1,7 +0,0 @@
-import { getGoogleSheetsData } from "./getSheets";
-
-export async function Sheets() {
-    const range = `Sheet1!A:E`;
-        const posts = await getGoogleSheetsData(range);
-        console.log(posts);
-}
